134ffa1b7e75a0db2e1843740607c9bad140e1f4
Nikema Prophet <nikema@gmail.com>
2019-07-24 08:49:53 -0700

    Linking movie cards to detail page using id parameter, linking home button on detail page

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -3,9 +3,8 @@ import axios from 'axios';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState({});
- 
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -18,7 +17,7 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
+  },[props.match.params.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
@@ -42,8 +41,7 @@ const Movie = (props) => {
           Metascore: <strong>{metascore}</strong>
         </div>
         <h3>Actors</h3>
-
-        {stars.map(star => (
+        {stars && stars.map(star => (
           <div key={star} className="movie-star">
             {star}
           </div>
